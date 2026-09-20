f0fbb434f28bca783f99830091ff6fc969dc915b
roderick-darell <tchinderoderick2006@gmail.com>
2026-09-20 17:26:22 +0200

    tp1 karatsuba 1

---
+++ 1/Karatsuba_PowerOf2-student.c
@@ -0,0 +1,164 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+/* Karatsuba algorithm where n is a power of two. 
+ * Based on a original code written by François Boulier. */
+
+void allocate_poly(double **A, int n)
+{
+  *A = (double *) malloc (n * sizeof (double));
+}
+
+void free_poly(double *A)
+{
+  free(A); 
+}
+
+void init_poly(double *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+      /* Tests performed with integer values for simplicity: */
+      A[i] = floor (10.0 * drand48 () - 5.0);
+    }
+}
+
+void print_poly (char *mesg, double *A, int n)
+{
+  int i;
+  printf ("%s ", mesg);
+  for (int i = 0; i < n; i++)
+    {
+      if (i == 0)
+        printf ("%ld ", (long) A[i]);
+      else if (i == 1)
+        printf ("%ld*x ", (long) A[i]);
+      else
+        printf ("%ld*x^%d ", (long) A[i], i);
+    }
+  printf ("\n");
+}
+
+bool compare_polys(double *A, double *B, int n)
+{
+  for (int i = 0; i < n; i++)
+    {
+      if (A[i] != B[i]) return false; 
+    }
+  return true; 
+}
+void add_poly (double *R, double *A, double *B, int n)
+{
+    for ( int i =0 ; i<n ; i++) {
+        R[i]= A[i] + B[i];
+        }
+
+}
+
+void sub_poly (double *R, double *A, double *B, int n)
+{
+     for ( int i =0 ; i<n ; i++) {
+            R[i]= A[i] - B[i];
+            }
+
+}
+
+void mul_poly (double *R, double *A, double *B, int n)
+{
+     int i, j;
+
+
+        for (i = 0; i < 2 * n - 1; i++)
+            R[i] = 0.0;
+
+
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                R[i + j] += A[i] * B[j];
+
+}
+
+void Karatsuba (double *R, double *A, double *B, int n)
+{
+  double *A0, *A1, *B0, *B1, *R0, *R2;
+  int p = n / 2;
+  double R1[n], A01[n], B01[n], R02[n], R3[n];
+  /*Other temporary polynomials can be defined here */
+  int i;
+    if (n == 1) {
+            R[0] = A[0] * B[0];
+            return;
+        }
+  else
+  for (i = 0; i < 2 * n - 1; i++)
+        R[i] = 0.0;
+    {/* General case: */
+      /* Decomposing polynomials A and B can be done in constant time  */
+      A0 = A;
+      A1 = A + p;
+      B0 = B;
+      B1 = B + p;
+
+      /* Polynomials R0 and R2 are sub-arrays of R (hence avoiding array copies) */
+      R0 = R;
+      R2 = R + n;
+
+      /* ... todo ... */
+    }
+Karatsuba(R0,A0,B0,p);
+Karatsuba(R2,A1,B1,p);
+add_poly (A01, A0, A1,p);
+add_poly (B01, B0, B1,p);
+add_poly (R02, R0, R2,n-1);
+Karatsuba(R3,A01,B01,p);
+sub_poly (R1, R3, R02,n-1);
+for (i = 0; i < n-1; i++)
+        R[i + p] += R1[i];
+
+
+}
+
+
+int main (int argc, char **argv)
+{
+  double *A = NULL; 
+  double *B = NULL; 
+  double *R_naive = NULL;
+  double *R_K = NULL;
+  
+  int n = 8;
+  if (argc > 1) { n = atoi(argv[1]); }
+
+  srand48 ((long) 42);
+
+  allocate_poly (&A, n);
+  allocate_poly (&B, n);
+  allocate_poly (&R_naive, 2*n-1);
+  allocate_poly (&R_K, 2*n-1);
+
+  init_poly(A, n);
+  init_poly(B, n);
+  print_poly ("A = ", A, n);
+  printf("\n");
+  print_poly ("B = ", B, n);
+  printf("\n");
+  
+  mul_poly (R_naive, A, B, n);
+  print_poly ("Naive: A * B = ", R_naive, 2*n-1);
+  printf("\n");
+  Karatsuba (R_K, A, B, n);
+  print_poly ("Karatsuba: A * B = ", R_K, 2*n-1);
+  printf("\n");
+
+  if (compare_polys(R_naive, R_K, 2*n-1))
+    printf("=> The two results match!\n");
+  else
+    printf("=> The two results do NOT match!\n");
+
+  free_poly(A);
+  free_poly(B);
+  free_poly(R_naive);
+  free_poly(R_K);
+}
---
+++ 1/rendu_act1.md
@@ -0,0 +1,5 @@
+1)  LA TAILLE DE A+B EST N 
+    SON DEGRE EST N-1
+    l'ordre de grandeur du nombre d'operation arithmetique est n 
+2) le degre de ab est 2(n-1)  sa taille est 2n-1 on a eu besoinn de nau carre operation
+    
